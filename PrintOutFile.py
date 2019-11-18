89963e6484cca59d02647624217524cd5ff63d96
Mxyzptlk <mxyzptlk@yeah.net>
2019-11-18 22:18:21 +0800

    update

--- PrintOutFile.py
+++ PrintOutFile.py
@@ -8,18 +8,25 @@ import win32api
 import win32print
 
 import os
-path_prefix = os.getcwd()
-filename = path_prefix + '\\套表模板' + '\\' + 'WN-QR-0-3-A软件及升级包杀毒记录-1.5.docx'
 
-win32api.ShellExecute(
-    0,
-    "print",
-    filename,
-    #
-    # If this is None, the default printer will
-    # be used anyway.
-    #
-    '/d:"%s"' % win32print.GetDefaultPrinter(),
-    ".",
-    0
-)
+
+def print_file():
+    path_prefix = os.getcwd()
+    filename = path_prefix + '\\套表模板' + '\\' + 'WN-QR-0-3-A软件及升级包杀毒记录-1.5.docx'
+    
+    win32api.ShellExecute(
+        0,
+        "print",
+        filename,
+        #
+        # If this is None, the default printer will
+        # be used anyway.
+        #
+        '/d:"%s"' % win32print.GetDefaultPrinter(),
+        ".",
+        0
+    )
+
+
+printer_name = win32print.GetDefaultPrinter()
+print(printer_name)
